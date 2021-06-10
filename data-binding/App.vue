ece502f8a825f36b1d62c0b776cb207eaa3f1aee
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-10 09:10:16 -0300

    Add, conceitos iniciais

---
+++ data-binding/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}</h1>
+    <input type="text" v-model="name" />
+
+    <br />
+
+    <a :href="link">link para o Google</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: "Eduardo",
+    link: "https://www.google.com",
+  }),
+};
+</script>
---
+++ diretivas/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Lista de tarefas!</h1>
+
+    <button @click="() => (showList = !showList)">Ver a lista</button>
+
+    <br />
+
+    <ul v-if="showList">
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ tasks[0].name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    showList: false,
+    tasks: [{ name: "Fazer o curso", isDone: false }],
+  }),
+};
+</script>
