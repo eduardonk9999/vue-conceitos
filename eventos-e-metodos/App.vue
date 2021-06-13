260151b8bc3972378f5f90812257d2ac3d7ddb69
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-13 19:07:33 -0300

    Add, metodos vue js

--- eventos-e-metodos/App.vue
+++ eventos-e-metodos/App.vue
@@ -7,10 +7,13 @@
     <br />
 
     <ul v-if="showList">
-      <li v-for="(task, index) in tasks" :key="index">
+      <li v-for="(task, index) in tasks" 
+        :key="index"
+        @dblclick="complete(task)"
+      >
         {{ tasks[0].name }}
-
-        <button>&times;</button>
+        <button
+          @click="remove(task)">&times;</button>
       </li>
     </ul>
   </div>
@@ -23,5 +26,19 @@ export default {
     showList: false,
     tasks: [{ name: "Fazer o curso", isDone: false }],
   }),
+  methods: {
+    complete (task) {
+      this.tasks = this.task.map(t => {
+        if (t.name === task.name) {
+          return {...t, isDone: t.isDone}
+        }
+        return { ...t}
+      })
+    },
+    remove(task){
+      this.tasks = this.tasks.filter(t => t.name !== task.name)
+      console.log('task', task)
+    }
+  },
 };
 </script>
